1e32cf5c5ff934bc7be9690127ed84347959a006
Donald Thevalingam <100483314+nnlnr@users.noreply.github.com>
2026-06-13 11:33:43 -0500

    new project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Donald Thevalingam on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
